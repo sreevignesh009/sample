5f309900eb69040a5d59a13757c3c3546394a15e
Sree Vignesh <sreevignesh009@gmail.com>
2026-06-23 19:39:31 +0530

    add ListDemo.java

---
+++ ListDemo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ListDemo {
+
+    public static void main(String[] args) {
+
+        List<String> names = new ArrayList<>();
+
+        names.add("Sri Vignesh");
+        names.add("Sathish");
+        names.add(1);
+
+        System.out.println(names);
+    }
+}
